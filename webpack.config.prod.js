327ecb9d4bdfc8f924ede53638cd1e533b24692e
Square Root of Saturn <sqrt@octoblu.com>
2016-08-19 16:23:10 -0700

    Better Webpack Builds

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,14 @@
 var path         = require('path');
 var webpack      = require('webpack');
 var autoprefixer = require('autoprefixer');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+// Assert this just to be safe.
+// Development builds of React are slow and not intended for production.
+if (process.env.NODE_ENV !== "production") {
+  throw new Error('Production builds must have NODE_ENV=production.');
+}
 
 module.exports = {
   devtool: 'cheap-module-source-map',
@@ -10,6 +18,17 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
+  },
+  output: {
+    // The build folder.
+    path: path.join(__dirname, 'dist'),
+    // Generated JS file names (with nested folders).
+    // There will be one main bundle, and one file per asynchronous chunk.
+    // We don't currently advertise code splitting but Webpack supports it.
+    filename: 'static/js/[name].[chunkhash:8].js',
+    chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
+    // We inferred the "public path" (such as / or /my-project) from homepage.
+    publicPath: '/'
   },
   resolve: {
     extensions: ['', '.js', '.jsx', '.json'],
@@ -23,13 +42,41 @@ module.exports = {
         'NODE_ENV': JSON.stringify('production')
       }
     }),
-    new webpack.NoErrorsPlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
+    // Generates an `index.html` file with the <script> injected.
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.join(__dirname, 'index.html'),
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      }
+    }),
+    // This helps ensure the builds are consistent if source hasn't changed:
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    // Try to dedupe duplicated modules, if any:
+    new webpack.optimize.DedupePlugin(),
+    // Minify the code.
     new webpack.optimize.UglifyJsPlugin({
       compress: {
+        screw_ie8: true, // React doesn't support IE8
         warnings: false
+      },
+      mangle: {
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+        screw_ie8: true
       }
-    })
+    }),
   ],
   module: {
     loaders: [
@@ -57,14 +104,33 @@ module.exports = {
         loader: 'json'
       },
       {
-        test: /\.(jpg|png|gif|eot|svg|ttf|woff|woff2)$/,
+        test: /\.(ico|jpg|png|gif|eot|otf|svg|ttf|woff|woff2)(\?.*)?$/,
+        exclude: /\/favicon.ico$/,
         include: [
           path.join(__dirname, 'src'),
           path.join(__dirname, 'node_modules')
         ],
         loader: 'file',
         query: {
-          name: '[name].[hash:8].[ext]'
+          name: 'static/media/[name].[hash:8].[ext]'
+        }
+      },
+      // A special case for favicon.ico to place it into build root directory.
+      {
+        test: /\/favicon.ico$/,
+        include: [ path.join(__dirname, 'src') ],
+        loader: 'file',
+        query: {
+          name: 'favicon.ico?[hash:8]'
+        }
+      },
+      // "html" loader is used to process template page (index.html) to resolve
+      // resources linked with <link href="./relative/path"> HTML tags.
+      {
+        test: /\.html$/,
+        loader: 'html',
+        query: {
+          attrs: ['link:href'],
         }
       }
     ]
